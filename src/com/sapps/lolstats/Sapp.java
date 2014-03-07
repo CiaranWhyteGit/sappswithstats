74fd271080c8020062b39c2183e7dc449d72f1d3
ciaran whyte <ciaranwhyte@ciarans-MacBook-Pro.local>
2014-03-07 15:09:01 +0000

    Fixing Sapp constructor

--- src/com/sapps/lolstats/Sapp.java
+++ src/com/sapps/lolstats/Sapp.java
@@ -4,8 +4,6 @@ package com.sapps.lolstats;
  * Created by ciaranwhyte on 06/03/2014.
  */
 public class Sapp {
-
-    public final String EUW = "euw";
     public String summonerId, region, summoner_name;
 
     public Sapp(String summoner_name, String region) {
@@ -18,12 +16,7 @@ public class Sapp {
     }
 
     public Sapp(String summoner_name) {
-        this.region = EUW;
-        this.summoner_name = summoner_name;
-        this.summonerId = getSummonerId(summoner_name, region);
-        FiveVsFiveRankedStats fiveVsFiveRankedStats = new FiveVsFiveRankedStats(this.summonerId,region);
-        FiveVsFiveUnRankedStats fiveVsFiveUnRankedStats = new FiveVsFiveUnRankedStats(this.summonerId,region);
-        RecentGameStats recentGameStats = new RecentGameStats(this.summonerId,region);
+        this(summoner_name, "euw");
     }
 
     private String getSummonerId(String summoner_name, String region) {
