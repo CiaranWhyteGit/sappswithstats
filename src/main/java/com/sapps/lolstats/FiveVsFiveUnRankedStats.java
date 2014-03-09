ac82758404c028a5bcfcb6eca88a3dc4f1845dbf
ciaranwhytegit <ciaran.whyte@gmail.com>
2014-03-09 22:18:31 +0000

    Adding Maven framework support This will probably make things messy

---
+++ src/main/java/com/sapps/lolstats/FiveVsFiveUnRankedStats.java
@@ -0,0 +1,9 @@
+package com.sapps.lolstats;
+
+/**
+ * Created by ciaranwhyte on 07/03/2014.
+ */
+public class FiveVsFiveUnRankedStats {
+    public FiveVsFiveUnRankedStats(String summonerId, String region, RiotRestClient riotRestClient) {
+    }
+}
