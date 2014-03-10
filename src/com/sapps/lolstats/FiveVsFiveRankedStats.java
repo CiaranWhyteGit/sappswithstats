8510836e52b64a2416447061c8417899d826cfea
ciaranwhytegit <ciaran.whyte@gmail.com>
2014-03-10 00:05:05 +0000

    Removing Main class from package

--- src/com/sapps/lolstats/FiveVsFiveRankedStats.java
+++
@@ -1,9 +0,0 @@
-package com.sapps.lolstats;
-
-/**
- * Created by ciaranwhyte on 06/03/2014.
- */
-public class FiveVsFiveRankedStats {
-    public FiveVsFiveRankedStats(String summonerId, String region, RiotRestClient riotRestClient ) {
-    }
-}
